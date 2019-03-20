b515b06c9bea5ad54dd9970a9a3410044ce89d03
Lun <kwl188836@gmail.com>
2019-03-20 15:53:02 +0800

    amendment : change app 'item' to 'items' new added : items/models.py updated

---
+++ Tradable/items/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+	name        = models.CharField( max_length = 100)
+	description = models.TextField()
+	price       = models.DecimalField(decimal_places=2, max_digits=10000)
+	condition   = models.TextField()
+	createdDateTime = models.DateTimeField(auto_now=True)
