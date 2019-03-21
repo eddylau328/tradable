430a1a3a024c927e44094dee9063caa3bb071a28
Eddy <eddylau0328@gmail.com>
2019-03-22 03:33:09 +0800

    Added Login,Logout Func in users

--- Tradable/Tradable/urls.py
+++ Tradable/Tradable/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from pages.views import home_view, about_view, myprofile_view
 from items.views import item_list_all_view, item_create_view, item_dynamic_lookup_view
 from users.views import user_register_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +30,6 @@ urlpatterns = [
     path('items/create/', item_create_view, name='item_detail_view'),
     path('items/<int:item_id>/', item_dynamic_lookup_view, name='dynamic item view'),
     path('users/register/', user_register_view, name='user_register_view'),
+    path('users/login/', auth_views.LoginView.as_view(template_name='user_login.html'), name='login'),
+    path('users/logout/', auth_views.LogoutView.as_view(template_name='user_logout.html'), name='logout'),
 ]
--- Tradable/users/views.py
+++ Tradable/users/views.py
@@ -11,8 +11,8 @@ def user_register_view(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('home')
+            messages.success(request, f'Your account has been created! You can log in now!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
 
