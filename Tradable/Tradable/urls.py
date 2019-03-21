15803a19a0eacae16a856d1be0e97fce80b31c7d
Eddy <eddylau0328@gmail.com>
2019-03-22 01:36:33 +0800

    Modified the users.views, users.forms, users/templates/users_register.html, urls.py

--- Tradable/Tradable/urls.py
+++ Tradable/Tradable/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, about_view, myprofile_view
 from items.views import item_list_all_view, item_create_view, item_dynamic_lookup_view
-
+from users.views import user_register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +27,6 @@ urlpatterns = [
     path('myprofile/', myprofile_view, name='myprofile'),
     path('items/', item_list_all_view, name='item_list_all_view'),
     path('items/create/', item_create_view, name='item_detail_view'),
-    path('items/<int:item_id>/' , item_dynamic_lookup_view, name='dynamic item view')
+    path('items/<int:item_id>/', item_dynamic_lookup_view, name='dynamic item view'),
+    path('users/register', user_register_view, name='user_register_view'),
 ]
--- Tradable/users/views.py
+++ Tradable/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
+
+
+def user_register_view(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'user_register.html', context)
