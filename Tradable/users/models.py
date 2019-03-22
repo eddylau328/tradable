e29ba90f04b5c0eb22fae6cbfdca28a6760f2fca
Eddy <eddylau0328@gmail.com>
2019-03-22 10:54:03 +0800

    Users app make migrations and migrate

--- Tradable/users/models.py
+++ Tradable/users/models.py
@@ -2,7 +2,4 @@ from django.db import models
 
 # Create your models here.
 # User model to store all registered user profile
-class User(models.Model):
-	user_name        = models.CharField( max_length = 100)
-	userID = models.BigIntegerField()
-	user_email       = models.EmailField(max_length=254)
+
