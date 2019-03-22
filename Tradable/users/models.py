7bcfc5082421c0dc4f8fb245047470ef9e624068
Eddy <eddylau0328@gmail.com>
2019-03-22 15:12:32 +0800

    Added update user profile

--- Tradable/users/models.py
+++ Tradable/users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
+
 # Profile is a model that used to save the users description
-
-
 class Profile(models.Model):
     # OneToOneField can restrict one User to have one Profile
     # on_delete=models.CASCADE, when user is deleted, his/her profile is deleted
@@ -15,3 +15,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    # overwrite the save method
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+        # resize profile image
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
