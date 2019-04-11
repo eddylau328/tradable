bab43a10cca17b617f96b8e784e95e89780617dd
Eddy <eddylau0328@gmail.com>
2019-04-11 21:42:15 +0800

    Added forget password function

--- Tradable/Tradable/settings.py
+++ Tradable/Tradable/settings.py
@@ -150,4 +150,11 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'cuhktradable@gmail.com'
+EMAIL_HOST_PASSWORD = 'kengor1234'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
